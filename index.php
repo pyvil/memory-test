dc10ff7a500c8692c663ec847d46e669e5a0582c
pyvil <vitalik-1p@ukr.net>
2015-10-25 03:16:47 +0300

    parse folder to output pics

--- index.php
+++ index.php
@@ -3,8 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+
+    <script src = "http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src = "js/common.js"></script>
 </head>
 <body>
-
+    <?
+        $images = scandir("img/test");
+        foreach($images as $img) {
+            if ($img[0] != '.')
+                echo "<img src='img/test/{$img}'>";
+        }
+    ?>
 </body>
 </html>
