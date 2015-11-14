0c21c1b2976a2a4e8ccf1fff7b7e49c3d1f67293
vipya <vipya@smile.fr>
2015-11-15 01:26:27 +0200

    markup update

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
     <script src = "http://code.jquery.com/jquery-latest.min.js"></script>
     <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>-->
     <script src="js/howler.min.js"></script>
+    <script src = "js/Helper.js"></script>
+    <script src = "js/Modal.js"></script>
     <script src = "js/common.js"></script>
     <script>
         <?php
@@ -31,7 +33,7 @@
         });
     </script>
 </head>
-<body>
+<body id="levels">
 <div id="pyvil_level_container">
     <div class="pyvil_level_list"></div>
 </div>
